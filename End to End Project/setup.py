0455b7e14d6751401ee44030ed6a15bdb3bc7a4a
Introvertking <122341379+Introvertking@users.noreply.github.com>
2023-03-13 12:25:35 +0530

    Add files via upload

---
+++ End to End Project/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements(file_path:str)->List(str):
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = filr_obj.readlines()
+        requirements = [req.replace('\n',' ')for req in requirements]
+setup(
+    name = '',
+    version = '',
+    author = '',
+    author_email = 'abdullahsyed940@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirement.txt')
+    
+    
+    
+)
+    
+    
