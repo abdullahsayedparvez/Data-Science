11ac774117e9f1aba224ab0be8c162b113417230
Data Science Fresher <122341379+abdullahsayedparvez@users.noreply.github.com>
2024-03-10 18:06:29 +0530

    Delete final project related directory

--- final project related/flask python logic main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask , render_template , request , redirect , url_for
-app = Flask(__name__)
-@app.route('/')   # Variable rule--> <int:score>
-def homepage():
-    return render_template('stock_market_frontend_main.html')
-
-if __name__ == '__main__':
-    app.run(debug=True,port=8000)  
